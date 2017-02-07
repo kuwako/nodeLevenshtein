1967fc90bd0429187fd6366533431394c163192d
kuwako <m.kuwako0702@gmail.com>
2017-02-07 11:57:54 +0000

    readstreamにはまる

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ var lev = require('levenshtein'),
 
 console.log(lev('あああ', 'あいあ'));
 
-var res = readBtCsv();
-var rs = null;
-function readBtCsv() {
-    rs = fs.createReadStream('bt.csv')
-            .pipe(iconv.decodeStream('SJIS'))
-            .pipe(iconv.encodeStream('UTF-8'))
-            .pipe(csv.parse())
-            .pipe(csv.transform(function(record){
-                return record;
-            }));
-    rs.on('data', function(data) { console.log(data)} );
+fs.createReadStream('sample.csv')
+    .pipe(iconv.decodeStream('SJIS'))
+    .pipe(iconv.encodeStream('UTF-8'))
+    .pipe(csv.parse())
+    .pipe(csv.transform(function(record) {
+        readBtCsv(record);
+    }));
+
+function readBtCsv(sample) {
+    fs.createReadStream('bt.csv')
+        .pipe(iconv.decodeStream('SJIS'))
+        .pipe(iconv.encodeStream('UTF-8'))
+        .pipe(csv.parse())
+        .pipe(csv.transform(function(record) {
+            levenshtein(sample, record);
+        }));
+}
+
+function levenshtein(sample, bt) {
+    console.log(sample);
+    console.log(bt);
 }
